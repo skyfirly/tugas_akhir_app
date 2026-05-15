0ed31e1dff264716867c334d21d07f33b572ea9f
skyfirly <firliramadhani987@gmail.com>
2026-05-15 07:01:24 +0700

    Selesai Misi 1 dan 2

---
+++ resources/views/master.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Pelatihan Teknik</title>
+    <link href="<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow">
+        <div class="container">
+            <a class="navbar-brand fw-bold" href="#">Portfolio</a>
+            <div class="navbar-nav ms-auto">
+                <a class="nav-link" href="/">Latihan1</a>
+                <a class="nav-link" href="#">Latihan2</a>
+                <a class="nav-link" href="#">Latihan3</a>
+                <a class="nav-link" href="/detail">Latihan4</a>
+            </div>
+        </div>
+    </nav>
+    <div class="container mt-4">
+        @yield('konten_utama')
+    </div>
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends('master')
+@section('konten_utama')
+<div class="bg-light p-5 rounded">
+<h1>Selamat Datang!</h1>
+<p>Ini adalah konten dari Halaman Utama</p>
+</div>
