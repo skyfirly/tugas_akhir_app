ee67bb2e6f1d2f0e098cc95d563c941383454bf3
skyfirly <firliramadhani987@gmail.com>
2026-05-15 18:20:14 +0700

    NGEBENERIN ERORRRRR

--- resources/views/detail-produk.blade.php
+++ resources/views/detail-produk.blade.php
@@ -1,5 +1,5 @@
 @extends('master')
-@section('content')
+@section('konten_utama')
 <div class="container mt-5">
     <div class="text-center mb-5">
         <h1 class="display-4 fw-bold text-danger" style="font-family: 'Comic Sans MS', cursive;">My Favorite Barbie Movie</h1>
@@ -8,10 +8,10 @@
     <div class="row">
         <div class="col-md-4 mb-4">
         <div class="card h-100 shadow border-danger">
-        <img src="https://pin.it/32YE5BEzS" class="card-img-top" alt="Charm School" style="height: 350px;" object-fit; cover;>
+        <img src="{{ asset('img/charm.jpg') }}" class="card-img-top" style="height: 350px;" object-fit; contain; alt="Charm School" >
         <div class="card-body">
             <h5 class="card-title fw-bold text-danger">Princess Charm School</h5>
-            <p class="card-text"><strong>Detail:</strong>Film ini bercerita tentang Blair Willows yang terpilih masuk sekolah putri.</p>
+            <p class="card-text">Film ini bercerita tentang Blair Willows yang terpilih masuk sekolah putri.</p>
         </div>
         </div>
         </div>
@@ -20,7 +20,7 @@
     <div class="row">
         <div class="col-md-4 mb-4">
         <div class="card h-100 shadow border-danger">
-            <img src="https://pin.it/2M36hfoaU" class="card-img-top" alt="A Pony Tale" style="height: 350px;" object-fit; cover;>
+            <img src="{{ asset('img/pony.jpg') }}" class="card-img-top" style="height: 350px;" object-fit; contain;  alt="A Pony Tale" >
             <div class="card-body">
                 <h5 class="card-title fw-bold text-danger">Barbie and Her Sisters in A Pony Tale</h5>
                 <p card-text>Film ini bercerita tentang Barbie dan adik-adik perempuannya yang berlibur di Alpine Academy milik bibinya, Marlene.</p>
@@ -32,7 +32,7 @@
     <div class="row">
         <div class="col-md-4 mb-4">
         <div class="card h-100 shadow border-danger">
-            <img src="https://pin.it/2TisWQmUC" class="card-img-top" alt="Three Musketerrs" style="height: 350px;" object-fit; cover;>
+            <img src="{{ asset('img/musk.jpg') }}" class="card-img-top" style="height: 350px;" object-fit; contain; alt="Three Musketeers">
             <div class="card-body">
                 <h5 class="card-title fw-bold text-danger">Barbie and the Three Musketerrs</h5>
                 <p card-text>Film ini bercerita tentang Corinne seorang gadis desa dari Gascony yang bermimpi menjadi seorang Musketerrs yaitu pasukan pengawal khusus kerajaan wanita pertama di Pranciss.</p>
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdukController;
 Route::get('/', [ProdukController::class, 'index']);
-Route::get('/detail', [App\Http\Controllers\Controller::class, 'detail']);
+Route::get('/detail', [App\Http\Controllers\ProdukController::class, 'detail']);
